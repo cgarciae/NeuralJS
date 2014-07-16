76ad23e1d2e341113df1ae6bd0fd01137704ada4
cgarciae <cgarcia.e88@gmail.com>
2014-07-16 13:00:13 -0500

    Layer almost complete + Readme

--- README.md
+++ README.md
@@ -2,3 +2,46 @@ NeuralJS
 ========
 
 Neural Networks in JavaScript, inspired by PyBrain.
+
+##### fullConnection
+**Signature**
+`fullConnection :: Layer -> Layer -> Layer`
+
+**Expression**
+`fullConnection (la, lb) == lb`
+
+**Description**
+`fullConnection` connects all neurons in `la` to all neurons in `lb`, and returns `lb`. More specifically, for all `Neuron` `na` 
+in `la.neurons`, and for all `Neuron` `nb` in `lb.neurons`, `Weight (na, nb)` belongs to `na.targets` and `nb.sources`. 
+This is the classical connection between layers.
+
+**Usage**
+Since `fullConnection (la, lb)` returns `lb`, this function admits for chaining
+```
+fullConnection(la, lb);
+fullConnection(lb, lc);
+```
+is equivalent to
+```
+fullConnection (fullConnection(la, lb), lc);
+```
+##### fullConnectTo
+**Signature**
+`(Layer l) => l.fullConnectTo :: Layer -> Layer`
+
+**Expression**
+`la.fullConnectTo (lb) == fullConnection (la, lb) == lb`
+
+**Description**
+Same as `fullConnection` but wrapped as a method for `Layer`s.
+
+**Usage**
+`fullConnectTo` improves the chaining capacities of `fullConnection`, given that
+```
+fullConnection(la, lb);
+fullConnection(lb, lc);
+```
+is equivalent to
+```
+la.fullConnectTo (lb).fullConnectTo (lc);
+```
--- src/neural.js
+++ src/neural.js
@@ -6,6 +6,11 @@ var R = ramda,
     L = LambdaJS,
     N = {};
 
+function addSignals (acc, weight){
+    return acc + weight.w * weight.source.y;
+}
+var sumSignals = R.foldl (addSignals, 0);
+
 /**
  * @class
  * @property {number} val
@@ -14,50 +19,75 @@ function Val (val) {
     this.val = val;
 }
 N.Val = Val;
-    /**
+/**
  *
  * @class
  * @property {Val} w
- * @property {LinearNeuron} source
- * @property {LinearNeuron} target
- * @param {LinearNeuron} source
+ * @property {Neuron} source
+ * @property {Neuron} target
+ * @param {Neuron} source
+ * @param {Neuron} target
+ * @param {number} [val]
  */
-function LinearWeight (source, target, val) {
-
-    this.w = new Val (val || Math.random ());
+function Weight (source, target, val) {
+    this._w = new Val (val || Math.random ());
     this.source = source;
     this.target = target;
 }
-N.LinearWeight = LinearWeight;
+N.Weight = Weight;
+
+Object.defineProperty(Weight.prototype, 'w', {
+    get : function () {
+        return this._w.val;
+    },
+    set : function (value) {
+        this._w.val = value;
+    }
+});
 
 /**
  * @returns {number}
  */
-LinearWeight.prototype.filterSignal = function filterSignal() {
-    return this.source.f() * this.w.val;
-};
 
 /**
  * @class
  * @property {Val} z
  * @property {Val} _y
- * @property {LinearWeight[]} sources
- * @property {LinearWeight[]} targets
+ * @property {Weight[]} sources
+ * @property {Weight[]} targets
  */
-function LinearNeuron () {
+function Neuron () {
     this._y = new Val (undefined);
+    this._z = new Val (undefined);
     this.sources = [];
     this.targets = [];
 }
+N.Neuron =  Neuron;
 
-N.LinearNeuron =  LinearNeuron;
+Object.defineProperty(Neuron.prototype, 'z', {
+    get : function () {
+        return this._z.val || (this._z.val = sumSignals (this.sources));
+    },
+    set : function (value) {
+        this._z.val = value;
+    }
+});
+
+Object.defineProperty(Neuron.prototype, 'y', {
+    get : function () {
+        return this._y.val;
+    },
+    set : function (value) {
+        this._y.val = value;
+    }
+});
 
 /**
- * @param {LinearNeuron} other
+ * @param {Neuron} other
  */
 var connectNeurons = N.connectNeurons = function (a,b) {
-    var weight = new LinearWeight (a, b);
-    relate (a, b);
+    var weight = new Weight (a, b);
+    relate (a, b, weight);
 };
 
 /**
@@ -68,39 +98,67 @@ var connectNeurons = N.connectNeurons = function (a,b) {
 
 //Layers
 /**@class
- * @property {LinearNeuron[]} neurons
- * @property {LinearLayer[]} sources
- * @property {LinearLayer[]} targets
+ * @property {Neuron[]} neurons
+ * @property {Layer[]} sources
+ * @property {Layer[]} targets
  * @param {number} n
  */
-var LinearLayer = N.LinearLayer = function LinearLayer (n) {
-    this.neurons = R.map (createNeuron(LinearNeuron), R.range(0,n));
+function Layer (n) {
+    this.neurons = R.map (createNeuron(Neuron), R.range(0,n));
     this.sources = [];
     this.targets = [];
-};
+}
+N.Layer = Layer;
 
 /**
- * @param {LinearLayer} a
- * @param {LinearLayer} b
+ * @param {Layer} b
+ * @returns {Layer}
  */
-function FullConnection (a, b) {
+Layer.prototype.fullConnectTo = function fullConnectTo (b) {
+    return fullConnection (this, b);
+};
+
+Layer.prototype.activate = function activate () {
+    if (! this.neurons[0].y) {
+        this.sources.forEach (function (l) {
+            l.activate();
+        });
+        this.layerActivationFunction();
+    }
+};
+
+Layer.prototype.layerActivationFunction = function () {
+    this.neurons.forEach (function (n) {
+        n.y = n.z;
+    });
+};
+
+
+
+    /**
+ * @param {Layer} a
+ * @param {Layer} b
+ * @return {Layer}
+ */
+function fullConnection (a, b) {
     relate (a, b);
-    
+
     a.neurons.forEach (function (na) {
         b.neurons.forEach (function (nb) {
             connectNeurons (na, nb);
         });
     });
+
+    return b;
 }
 
-N.FullConnection = FullConnection;
+N.fullConnection = fullConnection;
 
-    function createNeuron (constructor) {
+function createNeuron (constructor) {
     return function () {
         return new constructor();
     }
 }
-
 //Activation Functions
 /**
  * @param {number} x
@@ -110,13 +168,10 @@ function sigmoid (x) {
     return 1 / (1 + Math.exp (-x));
 }
 
-function relate (a, b) {
-    a.targets.push (b);
-    b.sources.push (a);
+function relate (a, b, container) {
+    a.targets.push (container || b);
+    b.sources.push (container || a);
 }
 
-function addSignals (acc, weight){
-    return acc + weight.filterSignal();
-}
 
-var sumSignals = R.foldl (addSignals, 0);
+
